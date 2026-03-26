de512d36462f682341761a1616734ec36e38e060
Thiago Godoy <89321983+othiagomadruga@users.noreply.github.com>
2026-03-25 21:43:11 -0300

    Update client.ts
    https://todo-project-7488.onrender.com

--- web/src/api/client.ts
+++ web/src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3001/api', // Endpoints do Node.js
+  baseURL: import.meta.env.VITE_API_URL || 'https://todo-project-7488.onrender.com', // Endpoints do Node.js
   headers: {
     'Content-Type': 'application/json',
   },
